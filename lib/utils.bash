bffc9aa552637e3dc6a67a2722e96ddf2067d23a
Oleg Montak <oleg.montak@gmail.com>
2024-02-13 18:21:42 +0200

    Use versionsort.suffix to sort pre release versions before release
    Old order:
    
    3.42.1
    3.42.2
    4.0.0
    4.0.0-beta-1
    4.0.0-beta.2
    4.0.0-beta.3
    4.0.0-beta.4
    4.1.0
    
    New order:
    
    3.42.1
    3.42.2
    4.0.0-beta-1
    4.0.0-beta.2
    4.0.0-beta.3
    4.0.0-beta.4
    4.0.0
    4.1.0

--- lib/utils.bash
+++ lib/utils.bash
@@ -17,21 +17,16 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
-sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
-}
-
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
+list_github_tags_sorted() {
+	git -c 'versionsort.suffix=-beta' ls-remote --tags --refs --sort version:refname "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
 		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
-list_all_versions() {
+list_all_versions_sorted() {
 	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
 	# Change this function if tuist has other means of determining installable versions.
-	list_github_tags
+	list_github_tags_sorted
 }
 
 download_release() {
